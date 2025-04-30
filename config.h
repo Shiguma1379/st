7b9c270f898003bcbf2a34c06c63c1e57e52d494
shigma <3349963044@qq.com>
2025-05-01 00:46:30 +0800

    add theme

--- config.h
+++ config.h
@@ -93,6 +93,9 @@ char *termname = "st-256color";
  */
 unsigned int tabspaces = 8;
 
+/* bg opacity */
+float alpha = 0.9;
+
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
@@ -121,7 +124,7 @@ static const char *colorname[] = {
 	"#cccccc",
 	"#555555",
 	"gray90", /* default foreground colour */
-	"gray20", /* default background colour */
+	"#282a36", /* default background colour */
 };
 
 
